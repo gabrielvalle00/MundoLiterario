fde1df2805a26b73e252b9381ce09773b03d657b
Valle <gabrivalle8@gmail.com>
2023-12-11 12:39:55 -0300

    Paginas de login,cadastro e contato. Prontas, falta apenas os scrip

---
+++ models/cadastro.models.js
@@ -0,0 +1,19 @@
+const { realpathSync } = require("fs");
+
+class Cadastro {
+    constructor(body) {
+        this.id = body.id
+        this.nome = body.nome;
+        this.email = body.email;
+        this.telefone = body.telefone;
+        this.senha = body.senha;
+        this.confirmaSenha = body.confirmaSenha;
+        this.genero = body.genero;
+
+  
+    }
+
+    
+
+}
+module.exports = Cadastro;
